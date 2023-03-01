371456b21d02a27542553b475195c13e8233d156
Moritz Boeger <moritzboeger@outlook.de>
2023-03-01 13:06:08 +0900

    Finished jsonl preprocessing

--- well/preprocessing/intents.py
+++ well/preprocessing/intents.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+from well.params import *
 def process_intents(d: dict[list[dict]]) -> pd.DataFrame:
     inputs = pd.DataFrame(columns=["tag", "input"])
     outputs = pd.DataFrame(columns=["tag", "output"])
@@ -9,13 +10,17 @@ def process_intents(d: dict[list[dict]]) -> pd.DataFrame:
                 [inputs, pd.DataFrame([[intent["tag"], pattern]],columns=["tag", "input"])])
         for response in intent["responses"]:
             outputs = pd.concat([outputs, pd.DataFrame([[intent["tag"], response]],columns=["tag", "output"])])
-    return inputs.merge(outputs, on="tag").drop(columns="tag")
+    result_df = inputs.merge(outputs, on="tag")
+    result_df["knowledge"] = result_df["tag"]
+    result_df["knowledge"] = result_df["knowledge"].map(lambda text: "fact" if "fact" in text else text)
+    return result_df.drop(columns="tag")
 
-def create_preprocessed_file(path_original="/home/moritzb6626/code/Carlotaortizml/WELL-repository/raw_data/intents.json", path_target="/home/moritzb6626/code/Carlotaortizml/WELL-repository/preprocessed_data_level1/chats/preprocessed_intends.csv"):
-    pd.read_json("../raw_data/intents.json")
+def create_preprocessed_file(path_original=INTENTS_RAW_PATH, path_target=INTENTS_PROCESSED_PATH):
+    pd.read_json(path_original)
     with open(path_original, "r") as intent_file:
+        print(":true: preprocessing intents")
         d = json.load(intent_file)
         process_intents(d=d).to_csv(path_target)
 
-if __name__ == "main":
+if __name__ == "__main__":
     create_preprocessed_file()
--- well/preprocessing/merge_level1.py
+++ well/preprocessing/merge_level1.py
@@ -1,15 +1,14 @@
 import pandas as pd
-def create_full_preprocessed_csv(path_intend="/home/moritzb6626/code/Carlotaortizml/WELL-repository/preprocessed_data_level1/chats/preprocessed_intends.csv",
-                                 path_mental_health_faq = "/home/moritzb6626/code/Carlotaortizml/WELL-repository/preprocessed_data_level1/chats/preprocessed_mental_health_faq.csv",
-                                 path_youtube_conversations = "/home/moritzb6626/code/Carlotaortizml/WELL-repository/preprocessed_data_level1/chats/preprocessed_youtube-conversations.csv",
-                                 path_target="/home/moritzb6626/code/Carlotaortizml/WELL-repository/preprocessed_data_level1/chats/preprocessed_full.csv"):
+from well.params import *
+def create_full_preprocessed_csv(path_intend,
+                                 path_youtube_conversations,
+                                 path_target):
     intents_df = pd.read_csv(path_intend).drop(columns="Unnamed: 0")
-    mental_health_faq_df = pd.read_csv(path_mental_health_faq).drop(columns="Unnamed: 0")
     youtube_conversations_df = pd.read_csv(path_youtube_conversations).drop(columns="Unnamed: 0")
-
-    pd.concat([intents_df, mental_health_faq_df, youtube_conversations_df], ignore_index=True)\
+    pd.concat([intents_df, youtube_conversations_df], ignore_index=True)\
         .dropna()\
             .to_csv(path_target)
-
 if __name__ == "__main__":
-    create_full_preprocessed_csv()
+    create_full_preprocessed_csv(path_intend=INTENTS_PROCESSED_PATH,
+                                 path_youtube_conversations=YOUTUBE_CONVERSATIONS_PROCESSED_PATH,
+                                 path_target=FULL_PROCESSED_PATH)
