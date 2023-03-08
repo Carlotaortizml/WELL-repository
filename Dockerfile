ed18b7ccec7e8107459d1645fc92dc17e316c06b
Samneang Seng <hisamneang@gmail.com>
2023-03-08 11:30:31 +0900

    update url with docker image url

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY raw_data/DialoGPT-small /DialoGPT-small
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# RUN python -c "from transformers import AutoModel; AutoModel.from_pretrained('/DialoGPT-small')"
+RUN python -c "from transformers import AutoModel; AutoModel.from_pretrained('/DialoGPT-small')"
 
 CMD uvicorn well.api.main:app --host 0.0.0.0 --port $PORT
--- well/ml_logic/dialoGPT_model.py
+++ well/ml_logic/dialoGPT_model.py
@@ -3,11 +3,11 @@ import torch
 
 class DialoGPTModel:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("/Users/macbookpro/Desktop/WELL-repository/raw_data/DialoGPT-small")
-        self.model = AutoModelForCausalLM.from_pretrained("/Users/macbookpro/Desktop/WELL-repository/raw_data/DialoGPT-small")
+        # self.tokenizer = AutoTokenizer.from_pretrained("/Users/macbookpro/Desktop/WELL-repository/raw_data/DialoGPT-small")
+        # self.model = AutoModelForCausalLM.from_pretrained("/Users/macbookpro/Desktop/WELL-repository/raw_data/DialoGPT-small")
 
-        # self.tokenizer = AutoTokenizer.from_pretrained("/DialoGPT-small")
-        # self.model = AutoModelForCausalLM.from_pretrained("/DialoGPT-small")
+        self.tokenizer = AutoTokenizer.from_pretrained("/DialoGPT-small")
+        self.model = AutoModelForCausalLM.from_pretrained("/DialoGPT-small")
 
 
         #self.tokenizer = AutoTokenizer.from_pretrained("https://console.cloud.google.com/storage/browser/well-project-model-bucket/finetuned-model_2023-03-06")
